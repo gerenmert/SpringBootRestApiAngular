0b348b8adacc5406773e11155c62d4f65f2c1209
mertgeren <mert.geren@iqb.solutions>
2020-09-10 17:19:02 +0300

    customer resource (controller) added

---
+++ src/main/java/com/mertgeren/web/rest/CustomerResource.java
@@ -0,0 +1,85 @@
+package com.mertgeren.web.rest;
+
+import com.mertgeren.service.CustomerService;
+import com.mertgeren.service.dto.CustomerDTO;
+import com.mertgeren.web.rest.errors.BadRequestAlertException;
+import io.github.jhipster.web.util.HeaderUtil;
+import io.github.jhipster.web.util.PaginationUtil;
+import io.github.jhipster.web.util.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerResource {
+
+    private final Logger log = LoggerFactory.getLogger(CustomerResource.class);
+
+    private static final String ENTITY_NAME = "Customer";
+
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
+
+    private final CustomerService customerService;
+
+    public CustomerResource(CustomerService customerService) { this.customerService = customerService; }
+
+    @PostMapping("/customer")
+    public ResponseEntity<CustomerDTO> createCustomer(@Valid @RequestBody CustomerDTO customerDTO) throws URISyntaxException {
+        log.debug("REST request to save Customer : {}", customerDTO);
+        if (customerDTO.getId() != null) {
+            throw new BadRequestAlertException("A new tags cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        CustomerDTO result = customerService.save(customerDTO);
+        return ResponseEntity.created(new URI("/api/customer/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    @PutMapping("/customer")
+    public ResponseEntity<CustomerDTO> updateCustomer(@Valid @RequestBody CustomerDTO customerDTO) throws URISyntaxException {
+        log.debug("REST request to update Customer : {}", customerDTO);
+        if (customerDTO.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        CustomerDTO result = customerService.save(customerDTO);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, customerDTO.getId().toString()))
+            .body(result);
+    }
+
+    @GetMapping("/customers")
+    public ResponseEntity<List<CustomerDTO>> getAllCustomers(Pageable pageable) {
+        log.debug("REST request to get a page of Customers");
+        Page<CustomerDTO> page = customerService.findAll(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<CustomerDTO> getCustomer(@PathVariable Long id) {
+        log.debug("REST request to get Customer : {}", id);
+        Optional<CustomerDTO> customerDTO = customerService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(customerDTO);
+    }
+
+    @DeleteMapping("/customer/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        log.debug("REST request to delete Customer : {}", id);
+        customerService.delete(id);
+        return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, false, ENTITY_NAME, id.toString())).build();
+    }
+}
