0a14e74249115f0a35ff381d02f5a16b785cdac0
mertgeren <mert.geren@iqb.solutions>
2020-09-10 17:17:42 +0300

    customer service added

---
+++ src/main/java/com/mertgeren/service/CustomerService.java
@@ -0,0 +1,56 @@
+package com.mertgeren.service;
+
+import com.mertgeren.domain.Customer;
+import com.mertgeren.repository.CustomerRepository;
+import com.mertgeren.service.dto.CustomerDTO;
+import com.mertgeren.service.mapper.CustomerMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CustomerService {
+
+    private final Logger log = LoggerFactory.getLogger(CustomerService.class);
+
+    private final CustomerRepository customerRepository;
+
+    private final CustomerMapper customerMapper;
+
+    public CustomerService(CustomerRepository customerRepository, CustomerMapper customerMapper ) {
+        this.customerRepository = customerRepository;
+        this.customerMapper = customerMapper;
+    }
+
+    public CustomerDTO save(CustomerDTO customerDTO) {
+        log.debug("Request to save Customer : {}", customerDTO);
+        Customer customer = customerMapper.toEntity(customerDTO);
+        customer = customerRepository.save(customer);
+        return customerMapper.toDto(customer);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<CustomerDTO> findAll(Pageable pageable) {
+        log.debug("Request to get all Customers");
+        return customerRepository.findAll(pageable)
+            .map(customerMapper::toDto);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<CustomerDTO> findOne(Long id) {
+        log.debug("Request to get Customer : {}", id);
+        return customerRepository.findById(id)
+            .map(customerMapper::toDto);
+    }
+
+    public void delete(Long id) {
+        log.debug("Request to delete Customer : {}", id);
+        customerRepository.deleteById(id);
+    }
+}
