679b4fae2c3f0c85c8b1e4077d05929ba08db798
mertgeren <mert.geren@iqb.solutions>
2020-09-10 17:16:43 +0300

    customer repository added

---
+++ src/main/java/com/mertgeren/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.mertgeren.repository;
+
+import com.mertgeren.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
