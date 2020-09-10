93ddf024763d42f0783a517941695d813e6954c0
mertgeren <mert.geren@iqb.solutions>
2020-09-10 17:39:36 +0300

    customer and customer-list component added

---
+++ src/main/webapp/app/home/customer/customer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.scss'],
+})
+export class CustomerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 import { SpringBootRestApiAngularSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
   imports: [SpringBootRestApiAngularSharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CustomerComponent, CustomerListComponent],
 })
 export class SpringBootRestApiAngularHomeModule {}
