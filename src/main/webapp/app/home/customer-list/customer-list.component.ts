55ee6058e14aa7155d6b59a97bca76426ba02e4a
mertgeren <mert.geren@iqb.solutions>
2020-09-10 18:37:45 +0300

    customer-list component html and ts update

--- src/main/webapp/app/home/customer-list/customer-list.component.ts
+++ src/main/webapp/app/home/customer-list/customer-list.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomerService } from 'app/service/customer.service';
+import { Customer } from 'app/service/customer.model';
 
 @Component({
   selector: 'jhi-customer-list',
   templateUrl: './customer-list.component.html',
-  styleUrls: ['./customer-list.component.scss'],
+  styleUrls: ['./customer-list.component.css'],
 })
 export class CustomerListComponent implements OnInit {
-  constructor() {}
+  constructor(private httpClientService: CustomerService, private toastr: ToastrService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.httpClientService.customerList();
+  }
+
+  populateForm(customer: Customer) {
+    this.httpClientService.formData = Object.assign({}, customer);
+  }
+
+  onDelete(id: number) {
+    if (confirm('Are you sure to delete this record?')) {
+      this.httpClientService.deleteCustomer(id).subscribe(res => {
+        this.toastr.warning('The customer has been deleted successfully', 'Customer. Register');
+        this.httpClientService.customerList();
+      });
+    }
+  }
 }
